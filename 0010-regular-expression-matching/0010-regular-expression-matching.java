7d13a0178f91a99750b2b39d778fb0c84baa14e8
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-06-11 22:30:38 +0530

    Time: 20 ms (34.43%), Space: 43.8 MB (5.70%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp=new boolean[p.length()+1][s.length()+1];
+        for(int i=0;i<dp.length;i++){
+          for(int j=0;j<dp[0].length;j++){
+              if(i==0 && j==0){
+                  dp[i][j]=true;
+              }else if(i==0){
+                  dp[i][j]=false;
+              }else if(j==0){
+                  if(p.charAt(i-1)=='*'){
+                      dp[i][j]=dp[i-2][j];
+                  }else{
+                      dp[i][j]=false;
+                  }
+              }else{
+                  if(p.charAt(i-1)=='*'){
+                      if(dp[i-2][j]){
+                          dp[i][j]=true;
+                      }else if( (p.charAt(i-2)=='.' || p.charAt(i-2)==s.charAt(j-1))){
+                          dp[i][j]=dp[i][j-1];
+                      }
+                  }else if(p.charAt(i-1)=='.'||p.charAt(i-1)==s.charAt(j-1)){
+                      dp[i][j]=dp[i-1][j-1];
+                      
+                  }else{
+                      dp[i][j]=false;
+                  }
+              }
+          }
+        
+        }
+        for(boolean[] i:dp){
+            System.out.println(Arrays.toString(i));
+        }
+        return dp[p.length()][s.length()];
+    }
+
+}
