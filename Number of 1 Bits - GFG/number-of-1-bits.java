b0d393497688aed510077bec12aaa83719e4ad4e
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-06-19 17:37:22 +0530

    Added solution - LeetHub

---
+++ Number of 1 Bits - GFG/number-of-1-bits.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            int cnt = ob.setBits(N);
+            System.out.println(cnt);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int setBits(int n) {
+        // code here
+        int count=0;
+        while(n!=0){
+            int rmsb=n & -n;
+            n=n-rmsb;
+            count++;
+        }
+        return count;
+    }
+}
