81ce263a62c466e31e4d7f6298a2755b4fac01b2
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-06-22 17:55:12 +0530

    Time: 19 ms (48.19%), Space: 56.1 MB (28.79%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,28 +1,35 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n=prices.length;
-        int[][] dp=new int[n][2];
-        for(int[] i:dp){
-            Arrays.fill(i,-1);
+        int[][] dp=new int[n+1][2];
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy==1){
+                    dp[i][buy]= Math.max(-prices[i]-fee+dp[i+1][0],dp[i+1][1]);
+                 }else{
+                    dp[i][buy]=Math.max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                }
+        
+            }
         }
-        return help(prices,fee,0,1,dp);
+        return dp[0][1];
         
     }
-    public int help(int[] prices, int fee,int idx,int buy,int[][] dp){
-     if(idx==prices.length){
-         return 0;
-     }
-    if(dp[idx][buy]!=-1){
-        return dp[idx][buy];
-    }
-    if(buy==1){
-        return dp[idx][buy]= Math.max(-prices[idx]-fee+help(prices,fee,idx+1,0,dp),
-                        0+help(prices,fee,idx+1,1,dp));
-    }
+//     public int help(int[] prices, int fee,int idx,int buy,int[][] dp){
+//      if(idx==prices.length){
+//          return 0;
+//      }
+//     if(dp[idx][buy]!=-1){
+//         return dp[idx][buy];
+//     }
+//     if(buy==1){
+//         return dp[idx][buy]= Math.max(-prices[idx]-fee+help(prices,fee,idx+1,0,dp),
+//                         0+help(prices,fee,idx+1,1,dp));
+//     }
         
-        return dp[idx][buy]=Math.max(+prices[idx]+help(prices,fee,idx+1,1,dp),
-                        0+help(prices,fee,idx+1,0,dp));
+//         return dp[idx][buy]=Math.max(+prices[idx]+help(prices,fee,idx+1,1,dp),
+//                         0+help(prices,fee,idx+1,0,dp));
     
-    }
+//     }
    
 }
