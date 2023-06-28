1570f26e7e5ff6b80a60123e86dfc4b25087ccd0
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-06-28 12:27:27 +0530

    Added solution - LeetHub

---
+++ Copy Set Bits in Range - GFG/copy-set-bits-in-range.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line[] = in.readLine().trim().split("\\s+");
+            int x = Integer.parseInt(line[0]);
+            int y = Integer.parseInt(line[1]);
+            int l = Integer.parseInt(line[2]);
+            int r = Integer.parseInt(line[3]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.setSetBit(x, y, l, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int setSetBit(int x, int y, int l, int r){
+        // code here
+        int mask=1;
+        int masklen=r-l;
+        while(masklen>=1){
+            mask=(mask<<1)|1;
+            masklen--;
+        }
+        int shift=l-1;
+        mask=mask<<shift;
+        //System.out.println(Integer.toBinaryString(mask));
+        int masky=mask;
+        masky=masky&y;
+        int ans=masky|x;
+        return ans;
+    }
+}
