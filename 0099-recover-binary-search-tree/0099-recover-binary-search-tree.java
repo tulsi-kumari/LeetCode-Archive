ca1b98c8594e86694177aa74e5b7437974074e7b
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-07-05 19:52:22 +0530

    Time: 2 ms (100.00%), Space: 43.3 MB (55.70%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -19,31 +19,72 @@ class Solution {
     static TreeNode b=null;
     static boolean flag=false;
     public void recoverTree(TreeNode root) {
-        inorder(root);
+        inorderMorris(root);
         int temp=a.val;
         a.val=b.val;
         b.val=temp;
         prev=null;
         a=null;
         b=null;
-        flag=false;
     }
-    public void inorder(TreeNode root){
-        if(root==null){
-            return;
+    // public void inorder(TreeNode root){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     inorder(root.left);
+    //     if(prev==null){
+    //         prev=root;
+    //     }
+    //     if(prev.val>root.val){
+    //        if(!flag){
+    //            a=prev;
+    //            flag=true;
+    //        }
+    //        b=root;
+    //     }
+    //     prev=root;
+    //     inorder(root.right);
+    // }
+    public TreeNode findrmNode(TreeNode lm,TreeNode curr){
+        
+        while(lm.right!=null && lm.right!=curr){
+            lm=lm.right;
         }
-        inorder(root.left);
-        if(prev==null){
-            prev=root;
+        return lm;
+    }
+    public void inorderMorris(TreeNode root){
+        TreeNode curr=root;
+        
+        while(curr!=null){
+            TreeNode ln=curr.left;
+            if(ln==null){
+                
+                    if(prev!=null && prev.val>curr.val){
+                        if(a==null){
+                            a=prev;
+                        }
+                        b=curr;
+                    }
+
+                prev=curr;
+                curr=curr.right;
+            }else{
+                TreeNode rm=findrmNode(ln,curr);
+                if(rm.right==curr){
+                    rm.right=null;
+                    if(prev.val>curr.val){
+                        if(a==null){
+                            a=prev;
+                        }
+                        b=curr;
+                    }
+                    prev=curr;
+                    curr=curr.right;
+                }else{
+                    rm.right=curr;
+                    curr=curr.left;
+                }
+            }
         }
-        if(prev.val>root.val){
-           if(!flag){
-               a=prev;
-               flag=true;
-           }
-           b=root;
-        }
-        prev=root;
-        inorder(root.right);
     }
 }
