b7def7ac631db7cd0d9b6c7d9a5a87472c260820
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-07-06 12:50:26 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        if(low>high){
+            return;
+        }
+        // code here
+        int s=low;
+        int e=high;
+        int mid=s+(e-s)/2;
+        int pivot=arr[mid];
+        
+        while(s<=e){
+            while(arr[s]<pivot){
+                s++;
+            }
+            while(arr[e]>pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp=arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;
+                e--;
+            }
+        }
+        //System.out.println(Arrays.toString(arr)+" "+low+" "+high+" "+mid);
+        quickSort(arr,low,e);
+        quickSort(arr,s,high);
+    }
+    // static int partition(int arr[], int low, int high)
+    // {
+    //     // your code here
+    // } 
+}
