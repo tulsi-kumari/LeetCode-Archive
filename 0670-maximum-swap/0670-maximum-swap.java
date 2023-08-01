52fbda7d473f0fd4c2c6056e4710cd3454a7eda6
tulsisharmaa <102869380+tulsisharmaa@users.noreply.github.com>
2023-08-01 11:33:50 +0530

    Time: 13 ms (5.46%), Space: 40.4 MB (6.22%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -17,23 +17,41 @@ class Solution {
     }
     public int maximumSwap(int num) {
         String s=""+num;
-        PriorityQueue<Pair> q=new PriorityQueue<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            q.add(new Pair(ch-'0',i));
-        }
-        boolean visited[]=new boolean[s.length()];
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            while(visited[q.peek().idx]){
-                q.poll();
+        int[] rightmax=new int[s.length()];
+        int currmax=-1;
+        for(int i=s.length()-1;i>=0;i--){
+            int curr=s.charAt(i)-'0';
+            if(curr>currmax){
+                rightmax[i]=i;
+                currmax=curr;
+            }else{
+                rightmax[i]=rightmax[i+1];
             }
-            if(q.peek().x>ch-'0'){
-                Pair p=q.poll();
-                return Integer.parseInt(swap(i,p.idx,s));
-            }
-            visited[i]=true;
         }
+        for(int i=0;i<s.length();i++){
+            int curr=s.charAt(i)-'0';
+            int check=s.charAt(rightmax[i])-'0';
+            if(check>curr){
+                return Integer.parseInt(swap(i,rightmax[i],s));
+            }
+        }
+        // PriorityQueue<Pair> q=new PriorityQueue<>();
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     q.add(new Pair(ch-'0',i));
+        // }
+        // boolean visited[]=new boolean[s.length()];
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     while(visited[q.peek().idx]){
+        //         q.poll();
+        //     }
+        //     if(q.peek().x>ch-'0'){
+        //         Pair p=q.poll();
+        //         return Integer.parseInt(swap(i,p.idx,s));
+        //     }
+        //     visited[i]=true;
+        // }
         return num;
     }
     public String swap(int i,int j,String s){
