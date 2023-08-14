5a951d86d5df7c3b50749608b0b7c7ca5c340160
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2023-08-14 15:03:49 +0530

    Added solution - LeetHub

---
+++ Facing the sun - GFG/facing-the-sun.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] h = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                h[i] = Integer.parseInt(inputLine[i]);
+            }
+
+
+            int ans = new Solution().countBuildings(h, n);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int countBuildings(int h[], int n) {
+        // code here
+        Stack<Integer> st=new Stack<>();
+        for(int num:h)
+        {
+            if(st.isEmpty())
+            {
+                st.push(num);
+            }else{
+                if(st.peek()<num)
+                {
+                    st.push(num);
+                }
+            }
+        }
+        return st.size();
+    }
+}
