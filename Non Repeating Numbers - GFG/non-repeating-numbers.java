a28c79a93069ed0080d24e643ecb2466b598b26b
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2023-08-14 15:13:50 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        // Code here
+        int axorb=0;
+        for(int num:nums)
+        {
+            axorb=axorb^num;
+        }
+        int rmsb=axorb & -axorb ;
+        int x=0;
+        int y=0;
+        for(int val:nums)
+        {
+            if((val & rmsb) ==0)
+            {
+                x=x^val;
+            }else{
+                y=y^val;
+            }
+        }
+        int[] arr=new int[2];
+        arr[0]=Math.min(x,y);
+        arr[1]=Math.max(x,y);
+        return arr;
+    }
+}
