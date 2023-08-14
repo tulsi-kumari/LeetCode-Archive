5056832c5c2d16210877ad0d240784794ccad78f
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2023-08-14 15:17:52 +0530

    Time: 4 ms (97.78%), Space: 53.8 MB (97.81%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public static int findKthLargest(int[] arr, int k) {
+        return quickSelect(0,arr.length-1,k,arr);
+    }
+    public static int quickSelect(int left,int right,int k,int[] arr){
+        if(left>right){
+            return 0;
+        }
+        int s=left;
+        int e=right;
+        int mid=s+(e-s)/2;
+        int pivot=arr[mid];
+        while(s<e){
+            while(arr[s]<=pivot&&mid!=s){
+                s++;
+            }
+            while(arr[e]>pivot&&mid!=e){
+                e--;
+            }
+            if(s<=e){
+                if(s==mid){
+                    mid=e;
+                }else if(e==mid){
+                    mid=s;
+                }
+                swap(s,e,arr);
+            }
+        }
+        if(mid==arr.length-k){
+            return pivot;
+        }else if(mid<arr.length-k){
+            return quickSelect(mid+1,right,k,arr);
+        }
+        return quickSelect(left,mid-1,k,arr);
+
+    }
+    public static void swap(int i,int j,int[] arr){
+          int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
