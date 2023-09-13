9ca128e74766d7a18bd2d107e04056484441aff8
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2023-09-13 12:39:19 +0530

    Time: 18 ms (15.31%), Space: 45.5 MB (5.39%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,27 +1,18 @@
 class Solution {
     public int candy(int[] ratings) {
-        int i=0,n=ratings.length;
+        int i=1,n=ratings.length;
         
         int[] candies=new int[n];
-        int prev=-1;
+        candies[0]=1;
         while(i<n)
         {
-          if(i==0)
-          {
-            candies[i]=1;
-              prev=1;
-          }else
-          {
-              if(ratings[i]<=ratings[i-1])
-              {
-                  candies[i]=1;
-                  prev=1;
-              }else
-              {
-                  candies[i]=prev+1;
-                  prev++;
-              }
-          }
+            if(ratings[i]>ratings[i-1])
+            {
+                candies[i]=candies[i-1]+1;
+            }else
+            {
+                candies[i]=1;
+            }
             i++;
         }
         System.out.println(Arrays.toString(candies));
