a5e262e0bcafdc479bcd1d8c9b55522dd6e11500
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2023-10-04 14:26:37 +0530

    Time: 17 ms (65.95%), Space: 47.4 MB (70.51%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,21 +1,47 @@
 class MyHashMap {
-    int[] arr;
-    
+    private static final int SIZE = 1000;
+    private List<int[]>[] map;
+
     public MyHashMap() {
-        arr=new int[1000001];
-        Arrays.fill(arr,-1);
+        map = new ArrayList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            map[i] = new ArrayList<>();
+        }
     }
-    
+
     public void put(int key, int value) {
-        arr[key]=value;
+        int index = key % SIZE;
+        List<int[]> bucket = map[index];
+        for (int[] pair : bucket) {
+            if (pair[0] == key) {
+                pair[1] = value; 
+                return;
+            }
+        }
+        bucket.add(new int[]{key, value}); 
     }
-    
+
     public int get(int key) {
-        return arr[key];
+        int index = key % SIZE;
+        List<int[]> bucket = map[index];
+        for (int[] pair : bucket) {
+            if (pair[0] == key) {
+                return pair[1]; 
+            }
+        }
+        return -1; 
     }
-    
+
     public void remove(int key) {
-        arr[key]=-1;
+        int index = key % SIZE;
+        List<int[]> bucket = map[index];
+        for (int i = 0; i < bucket.size(); i++) {
+            int[] pair = bucket.get(i);
+            if (pair[0] == key) {
+                bucket.remove(i); 
+                return;
+            }
+        }
     }
 }
 
