3f02beb941ffc54b5d2a76380d5fe2c82c41931a
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2023-10-08 22:42:10 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node start, int key) {
+        // Add your code here.
+        Node p = start;
+        Node temp = new Node(key);
+        
+        //for first Node
+        if(key<p.data){
+            temp.next = p;
+            start = temp;
+            return start;
+        }
+        //rest of all nodes.
+        p = p.next;
+        Node prev = start;
+        while(p!=null){
+            if(key<p.data){
+                prev.next = temp;
+                temp.next = p;
+                return start;
+            }
+            prev = p;
+            p = p.next;
+        }
+        prev.next = temp;
+        return start;
+    }
+}
