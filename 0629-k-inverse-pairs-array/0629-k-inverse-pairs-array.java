3e9fed718c8e97b17b5acea0e89d96c1e770d301
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2024-01-27 17:15:33 +0530

    Time: 1785 ms (13.45%), Space: 68.7 MB (14.29%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    private final int MOD = (int) (1e9 + 7);
+    private Integer dp[][];
+
+    public int kInversePairs(int n, int k) {
+        dp = new Integer[n + 1][k + 1];
+
+        return getInversions(n, k);
+    }
+
+    private int getInversions(int n, int k) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (k == 0) {
+            return 1;
+        }
+
+        if (dp[n][k] != null) {
+            return dp[n][k];
+        }
+
+        int result = 0;
+
+        for (int inversion = 0; inversion <= Math.min(k, n - 1); inversion++) {
+            result += getInversions(n - 1, k - inversion);
+            result %= MOD;
+        }
+
+        return dp[n][k] = result;
+    }
+}
