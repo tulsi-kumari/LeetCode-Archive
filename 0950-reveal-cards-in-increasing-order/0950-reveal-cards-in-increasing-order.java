3c63288b7840b0a776ee959394dfbb0f47a98b2f
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2024-04-10 14:36:45 +0530

    Time: 3 ms (97.57%), Space: 43 MB (90.78%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n=deck.length;
+        int[] ans=new int[n];
+        
+        int i=0;
+        boolean flag=false;
+        int j=0;
+        while(i<n)
+        {
+            if(ans[j]==0)
+            {
+                if(!flag)
+                {
+                    ans[j]=deck[i];
+                    i++;
+                    flag=true;
+                    
+                }else
+                {
+                    flag=false;
+                }
+            }
+            j=(j+1)%n;
+        }
+        return ans;
+    }
+}
